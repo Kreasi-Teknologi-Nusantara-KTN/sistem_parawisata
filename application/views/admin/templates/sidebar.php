bbcc71a7eb31c11b24ea2d55c282498ae66c8d3a
febriananur <febriananurokhman@gmail.com>
2021-12-09 14:14:32 +0700

    fix icon

--- application/views/admin/templates/sidebar.php
+++ application/views/admin/templates/sidebar.php
@@ -7,7 +7,10 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+              <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon">
+                   <i class="fas fa-user-shield"></i>
+                </div>
                 <div class="sidebar-brand-text mx-3">Admin</div>
             </a>
 
--- application/views/pengelola/templates/sidebar.php
+++ application/views/pengelola/templates/sidebar.php
@@ -8,9 +8,11 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon">
+                   <i class="fas fa-user-tie"></i>
+                </div>
                 <div class="sidebar-brand-text mx-3">Pengelola</div>
             </a>
-
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
