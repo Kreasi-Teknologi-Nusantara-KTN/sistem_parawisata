e8a3b53dbcf2d6614ec22e81b43f25103b0f7d8f
febriananur <febriananurokhman@gmail.com>
2021-12-11 12:52:40 +0700

    tambah auth(progress)

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,10 +1,54 @@
 <?php
 
-class Login extends CI_Controller{
-    public function index(){
-        $this->load->view('templates/header');
-        $this->load->view('login/index');
-        $this->load->view('templates/footer');
+class Login extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Login';
+        $this->load->view('login/templates/header', $data);
+        $this->load->view('login/login');
+        $this->load->view('login/templates/footer');
+    }
+
+    public function register()
+    {
+        $this->form_validation->set_rules('nama', 'Nama', 'required|trim');
+        $this->form_validation->set_rules(
+            'email',
+            'Email',
+            'required|trim|valid_email]'
+        );
+        $this->form_validation->set_rules('jenkel', 'Jenkel', 'required');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required|trim');
+        $this->form_validation->set_rules(
+            'password1',
+            'Password',
+            'required|trim|matches[password2]|min_length[4]',
+            [
+                'matches' => 'password tidak sama',
+                'min_lenght' => 'Password terlalu pendek',
+            ]
+        );
+        $this->form_validation->set_rules(
+            'password2',
+            'Password',
+            'required|trim|matches[password1]'
+        );
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Registrasi';
+            $this->load->view('login/templates/header', $data);
+            $this->load->view('login/register');
+            $this->load->view('login/templates/footer');
+        } else {
+           ''
+        }
     }
 }
 
